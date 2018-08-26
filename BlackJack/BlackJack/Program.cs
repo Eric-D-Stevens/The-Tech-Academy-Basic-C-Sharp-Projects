8cfefdc08900d12c6a6f5dd40a3535c5510b0202
Eric Stevens <ericstevens26101@gmail.com>
2018-08-26 12:03:03 -0700

    add function overloading

--- BlackJack/BlackJack/Program.cs
+++ BlackJack/BlackJack/Program.cs
@@ -12,28 +12,41 @@ namespace BlackJack
         {
 
             Deck deck = new Deck();
-            deck = Shuffle(deck);
+            int timesShuffled = 0;
+            deck = Shuffle(deck, out timesShuffled, 3);
 
+
+
+            // Print Deck
             foreach(Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
+            Console.WriteLine("Cards In Deck: {0}", deck.Cards.Count);
+            Console.WriteLine("Times Shuffled: {0}", timesShuffled);
 
-            Console.WriteLine(deck.Cards.Count);
+
+            // Hold
             Console.Read();
         }
 
-        public static Deck Shuffle(Deck deck)
+
+        public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 1)
         {
-            List<Card> temp = new List<Card>();
-            Random random = new Random();
-            while(deck.Cards.Count > 0)
+            timesShuffled = 0;
+            for(int i = 0; i < times; i++)
             {
-                int randomIndex = random.Next(0, deck.Cards.Count);
-                temp.Add(deck.Cards[randomIndex]);
-                deck.Cards.RemoveAt(randomIndex);
+                timesShuffled++;
+                List<Card> temp = new List<Card>();
+                Random random = new Random();
+                while (deck.Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, deck.Cards.Count);
+                    temp.Add(deck.Cards[randomIndex]);
+                    deck.Cards.RemoveAt(randomIndex);
+                }
+                deck.Cards = temp;
             }
-            deck.Cards = temp;
             return deck;
         }
     }
