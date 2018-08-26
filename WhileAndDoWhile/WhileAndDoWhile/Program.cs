8c245d4bfed90abe6f454abd5fd120bbd2f995bc
Eric Stevens <ericstevens26101@gmail.com>
2018-08-25 21:19:31 -0700

    Add While and Do While Project

---
+++ WhileAndDoWhile/WhileAndDoWhile/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileAndDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // While Loop
+            Console.Write("Enter a number to count to: ");
+            int count = int.Parse(Console.ReadLine());
+            int i = 0;
+            while(i <= count)
+            {
+                Console.Write(i + " ");
+                i++;
+            }
+
+            // Do While Loop
+            Console.Write("\n\nGuess a number between 1 and 10: ");
+            int guess = int.Parse(Console.ReadLine());
+            int answer = 3;
+            do
+            {
+                if (guess != answer)
+                {
+                    Console.Write("\nWrong, guess again: ");
+                    guess = int.Parse(Console.ReadLine());
+                }
+            } while (guess != answer);
+        }
+    }
+}
