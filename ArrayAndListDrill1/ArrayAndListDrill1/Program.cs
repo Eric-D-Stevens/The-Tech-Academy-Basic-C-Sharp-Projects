fd3cc0403ad1f5d1e4afa21d092a3c1a89b933c5
Eric Stevens <ericstevens26101@gmail.com>
2018-08-25 22:43:49 -0700

    Add array and list drill 1

---
+++ ArrayAndListDrill1/ArrayAndListDrill1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAndListDrill1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare and initialize arrays
+            string[] strArr = { "red", "green", "blue", "yellow", "orange", "magenta", "black", "brown", "grey", "purple" };
+            int[] intArr = {10,9,8,7,6,5,4,3,2,1};
+
+            // Get user input
+            // Color
+            Console.Write("Enter a number between 1 and 10 for a color: ");
+            int colorIn = int.Parse(Console.ReadLine());
+            if(colorIn > 9 || colorIn < 0)
+            {
+                Console.WriteLine("Index out of array bounds");
+                Console.Read();
+                return;
+            }
+            Console.WriteLine("Color: " + strArr[colorIn - 1]);
+            // Number
+            Console.Write("\nEnter a number between 1 and 10 for a number: ");
+            int numberIn = int.Parse(Console.ReadLine());
+            if (numberIn > 9 || numberIn < 0)
+            {
+                Console.WriteLine("Index out of array bounds");
+                Console.Read();
+                return;
+            }
+            Console.WriteLine("number: " + intArr[numberIn - 1]);
+
+            // List of strings
+            List<string> mylist = new List<string>(new string[] 
+                { "Bear", "Puffin", "Spider", "Fish", "Monkey"});
+            Console.Write("\nEnter a number between 1 and 5 for an Animal: ");
+            numberIn = int.Parse(Console.ReadLine());
+            Console.WriteLine("Animal: " + mylist[numberIn - 1]);
+            Console.Read();
+        }
+    }
+}
---
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Array Declarations
+            int[] numArray = new int[5];
+            numArray[0] = 5;
+            numArray[1] = 123;
+            numArray[2] = 1234134;
+            numArray[3] = 2324;
+            numArray[4] = 23424;
+            numArray[5] = 23;
+
+            int[] arr1 = new int[] { 1, 2, 5, 6, 84564, 456 };
+            int[] arr2 = {1221, 123, 234, 345, 3453, 3453}
+
+            // Lists
+            List<int> intList = new List<int>();
+            intList.Add(2);
+            intList.Add(10);
+            intList.Remove(10);
+
+
+
+
+        }
+    }
+}
