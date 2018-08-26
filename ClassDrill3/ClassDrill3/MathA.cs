b3e7f16ce0cbd4c8d6b99384898d4f26d6f1000f
Eric Stevens <ericstevens26101@gmail.com>
2018-08-26 13:04:55 -0700

    add class drill 3, function overloading by type

---
+++ ClassDrill3/ClassDrill3/MathA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill3
+{
+    class MathA
+    {
+        public int MltBy(int A) { return A * 3; }
+        public int MltBy(decimal A) { return Convert.ToInt32(A * 7); }
+        public int MltBy(string A) { return Convert.ToInt32(A) * 11; }
+
+    }
+}
---
+++ ClassDrill3/ClassDrill3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate and initialize class
+            MathA math = new MathA();
+            
+
+            // run overloaded methods
+            Console.WriteLine("Int: "+math.MltBy(12));
+            Console.WriteLine("Dec: "+math.MltBy(Convert.ToDecimal(12.00)));
+            Console.WriteLine("Str: "+math.MltBy("12"));
+
+            // hold
+            Console.Read();
+        }
+    }
+}
