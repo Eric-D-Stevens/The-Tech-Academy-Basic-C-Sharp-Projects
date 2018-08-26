f1c10ccfd3423888c2933f2f6cc4130fb42b266a
Eric Stevens <ericstevens26101@gmail.com>
2018-08-26 09:37:08 -0700

    Add try/catch drill 1

---
+++ ExceptionHandeling1/ExceptionHandeling1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandeling1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int> { 21, 11, 17, 12, 66, 77, 34, 754, 3234, 777, 99 };
+            
+
+            try
+            {
+                Console.WriteLine("Please enter a number to divide list values by: ");
+                int userIn = int.Parse(Console.ReadLine());
+                foreach (int num in intList)
+                {
+                    Console.WriteLine(num / userIn);
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.Read();
+                return;
+            }
+
+            finally
+            {
+                Console.WriteLine("Exiting try/catch statement without exception");
+                Console.ReadLine();
+            }
+        }
+    }
+}
