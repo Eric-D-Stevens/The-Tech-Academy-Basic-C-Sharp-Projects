b775a49c7b2d1f24c560283f85894ab797b80539
Eric Stevens <ericstevens26101@gmail.com>
2018-08-26 12:21:36 -0700

    Add classes drill 2

---
+++ ClassDrill2/ClassDrill2/MathAB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill1
+{
+    public class MathAB
+    {
+        public int AddAB(int A, int B) { return A + B; } 
+        public int SubAB(int A, int B) { return A - B; }
+        public int MltAB(int A, int B) { return A * B; }
+    }
+}
---
+++ ClassDrill2/ClassDrill2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Parameters by name
+            int a = 12;
+            int b = 13;
+
+            // Output Method Calls
+            MathAB math = new MathAB();
+            Console.WriteLine("A - B = " + math.SubAB(12, 13));
+            Console.WriteLine("A - B = " + math.SubAB(a, b));
+            
+            Console.Read();
+        }
+    }
+}
