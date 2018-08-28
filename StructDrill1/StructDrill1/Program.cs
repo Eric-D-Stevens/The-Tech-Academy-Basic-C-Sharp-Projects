cd3b36c8dbcb01747175d3dd70c07ff121be1151
Eric Stevens <ericstevens26101@gmail.com>
2018-08-27 21:12:06 -0700

    add struct drill 1

---
+++ StructDrill1/StructDrill1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructDrill1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number x = new Number();
+            x.Amount = 123.123M;
+        }
+
+    }
+
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+}
